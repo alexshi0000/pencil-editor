5ca254bb41e6728e7f2b0c806f6cd0bf417d9ef6
alexshi0000 <yuanshi948@gmail.com>
2020-06-07 21:02:41 -0400

    headings

--- public/editor.js
+++ public/editor.js
@@ -9,10 +9,19 @@ app.controller('controller', function($scope) {
   }
 
   $scope.init = function(doc) {
+    //document.getElementById(`loading`).className = ''
+    //document.getElementById(`editor`).className = 'hidden'
     $scope.database.ref(`user/${$scope.user.uid}/document/1`).once('value')
       .then(function(snapshot) {
-	document.getElementById('medium-title').innerHTML = snapshot.val().title
-	document.getElementById('medium-paragraph').innerHTML = snapshot.val().paragraph
+	//mock
+	let throttle = Math.floor(Math.random() * 1000)
+	setTimeout(load, 1000 + throttle)
+	function load() {
+	  //document.getElementById(`loading`).className = 'hidden'
+	  //document.getElementById(`editor`).className = ''
+	  document.getElementById('medium-title').innerHTML = snapshot.val().title
+	  document.getElementById('medium-paragraph').innerHTML = snapshot.val().paragraph
+	}
       });
   }
 
